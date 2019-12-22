d7e05c22942e4599b3e8edd4f57ffabae2e77446
Henrique Dias <mrhdias@gmail.com>
2019-12-22 11:33:42 +0000

    Test rewritten again from scratch

--- tests/get.nim
+++ tests/get.nim
@@ -1,26 +1,41 @@
 #
 # nim c -d:ssl -p=./ -r tests/get.nim 
 #
-import woocommerce/AsyncAPI
+import os
+import woocommerce/API
 import tables
 import json
 
-proc main() {.async.} =
-  let wcapi = await API(
-    url="http://example.com",
-    consumer_key="ck_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    consumer_secret="cs_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-  )
+proc main() =
+
+    if not (existsEnv("WCAPI_URL") and existsEnv("WCAPI_CONSUMER_KEY") and existsEnv("WCAPI_CONSUMER_SECRET")):
+        echo """
+Set manually the following environment variables to run the tests:
+export WCAPI_URL=http://example.com
+export WCAPI_CONSUMER_KEY=ck_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+export WCAPI_CONSUMER_SECRET=cs_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+"""
+        quit()
+
+    let url = getEnv("WCAPI_URL")
+    let consumer_key = getEnv("WCAPI_CONSUMER_KEY")
+    let consumer_secret = getEnv("WCAPI_CONSUMER_SECRET")
+
+    let wcapi = API(
+        url=url,
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret
+    )
   
-  let response = await wcapi.get("products", params = {"status": "publish", "per_page": $(10), "page": $(1)}.toTable)
-  echo response.status
-  echo response.version
-  echo response.headers["content-type"]
-  if response.status == "200 OK":
-    let products = parseJson(await response.body)
-    for product in products:
-      echo "SKU:", product["sku"], " NAME:", product["name"]
+    let response = wcapi.get("products", params = {"status": "publish", "per_page": $(10), "page": $(1)}.toTable)
+    echo response.status
+    echo response.version
+    echo response.headers["content-type"]
+    if response.status == "200 OK":
+        let products = parseJson(response.body)
+        for product in products:
+            echo "SKU:", product["sku"], " NAME:", product["name"]
 
-  wcapi.close()
+    wcapi.close()
 
-waitFor main()
+main()

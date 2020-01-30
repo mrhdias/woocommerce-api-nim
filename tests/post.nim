e5fb8821e73a0b0f372b6f9a71da9bece69516e7
Henrique Dias <mrhdias@gmail.com>
2020-01-30 15:18:30 +0000

    Removed imported tables library because is not used.

--- tests/post.nim
+++ tests/post.nim
@@ -3,55 +3,54 @@
 #
 import os
 import woocommerce/API
-import tables
 import json
 import rdstdin
 
 proc main() =
 
-    if not (existsEnv("WCAPI_URL") and existsEnv("WCAPI_CONSUMER_KEY") and existsEnv("WCAPI_CONSUMER_SECRET")):
-        echo """
+  if not (existsEnv("WCAPI_URL") and existsEnv("WCAPI_CONSUMER_KEY") and existsEnv("WCAPI_CONSUMER_SECRET")):
+    echo """
 Set manually the following environment variables to run the tests:
 export WCAPI_URL=http://example.com
 export WCAPI_CONSUMER_KEY=ck_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 export WCAPI_CONSUMER_SECRET=cs_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 """
-        quit()
+    quit()
 
-    let url = getEnv("WCAPI_URL")
-    let consumer_key = getEnv("WCAPI_CONSUMER_KEY")
-    let consumer_secret = getEnv("WCAPI_CONSUMER_SECRET")
+  let url = getEnv("WCAPI_URL")
+  let consumer_key = getEnv("WCAPI_CONSUMER_KEY")
+  let consumer_secret = getEnv("WCAPI_CONSUMER_SECRET")
 
-    let wcapi = API(
-        url=url,
-        consumer_key=consumer_key,
-        consumer_secret=consumer_secret
+  let wcapi = API(
+    url=url,
+      consumer_key=consumer_key,
+      consumer_secret=consumer_secret
     )
 
-    let data = %* {
-        "name": "TestAttribute",
-        "slug": "pa_testattribute",
-        "type": "select",
-        "order_by": "menu_order",
-        "has_archives": false
-    }
+  let data = %* {
+    "name": "TestAttribute",
+    "slug": "pa_testattribute",
+    "type": "select",
+    "order_by": "menu_order",
+    "has_archives": false
+  }
 
-    let input_response = readLineFromStdin "Create test attribute? [y/n]: "
+  let input_response = readLineFromStdin "Create test attribute? [y/n]: "
 
-    if input_response == "y" or input_response == "Y":
-        var response = wcapi.post("products/attributes", $data)
-        echo response.status
-        if response.status == "201 Created":
-            echo parseJson(response.body)
+  if input_response == "y" or input_response == "Y":
+    var response = wcapi.post("products/attributes", $data)
+    echo response.status
+    if response.status == "201 Created":
+      echo parseJson(response.body)
 
-        response = wcapi.get("products/attributes")
-        echo response.status
-        if response.status == "200 OK":
-            echo parseJson(response.body)
-    else:
-        echo "Test canceled!"
-        quit()
+    response = wcapi.get("products/attributes")
+    echo response.status
+    if response.status == "200 OK":
+      echo parseJson(response.body)
+  else:
+    echo "Test canceled!"
+    quit()
 
-    wcapi.close()
+  wcapi.close()
 
 main()
